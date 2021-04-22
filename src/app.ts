bd442d4f81be605fe9e2d85d40b191b3acfce847
khanr <khanriza@gmail.com>
2021-04-21 22:03:28 -0400

    additional setup items to app file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { Server } from "typescript-rest";
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+Server.buildServices(app);
+
+let port = parseInt(process.env.PORT || "");
+if (isNaN(port) || port === 0) {
+  port = 3000;
+}
+
+app.get("/", (req: Request, res: Response) => {
   res.send("hello");
 });
 
-app.listen(3000, () => console.log("server running"));
+app.listen(port, () => console.log("server running"));
