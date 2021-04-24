3399b166f55fd8e37bf3d66a3d5d9c0114a9dcad
khanr <khanriza@gmail.com>
2021-04-24 01:05:21 -0400

    database issues

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { Server } from "typescript-rest";
 
 // Importing all typescript-rest classes for wrapping Express paths
 import "./handlers";
+import { TryDBConnect } from "./helpers";
 
 const app: Application = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+  await TryDBConnect(() => {
+    res.json({
+      error: "Database connection error, please try again later",
+    });
+  }, next);
+});
 
 Server.buildServices(app);
 
