0eb9ea1c6103bfc2ec7e13929b86a035bba4c801
khanr <khanriza@gmail.com>
2021-04-25 16:13:33 -0400

    Added more confirgurations to user model

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column, Index } from "typeorm";
+import { IsEmail } from "class-validator";
 
 @Entity({ name: "user" })
 export class User {
@@ -6,16 +7,17 @@ export class User {
   id: number = 0;
 
   @Index({ unique: true })
-  @Column("varchar", { length: 500, nullable: true })
+  @Column("varchar", { length: 500 })
+  @IsEmail()
   email: string | null = null;
 
-  @Column("varchar", { length: 1000, nullable: true })
+  @Column("varchar", { length: 1000 })
   password: string = "";
 
-  @Column("varchar", { length: 50 })
+  @Column("varchar", { length: 50, nullable: true })
   firstName: string = "";
 
-  @Column("varchar", { length: 50 })
+  @Column("varchar", { length: 50, nullable: true })
   lastName: string = "";
 
   @Column("varchar", { length: 15000, nullable: true })
