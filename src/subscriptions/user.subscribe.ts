3eed11c3834fca0508b7a17d28ea59bf58537ab2
khanr <khanriza@gmail.com>
2021-04-25 20:47:52 -0400

    added additions to user subscription

--- src/subscriptions/user.subscribe.ts
+++ src/subscriptions/user.subscribe.ts
@@ -11,11 +11,19 @@ import * as bcrypt from "bcryptjs";
 
 @EventSubscriber()
 export class UserSubscriber implements EntitySubscriberInterface<User> {
+  listenTo() {
+    return User;
+  }
+
   async hashPassword(entity: User): Promise<void> {
     entity.password = await bcrypt.hash(entity.password, 10);
   }
 
   beforeInsert(event: InsertEvent<User>): Promise<void> {
+    return this.hashPassword(event.entity);
+  }
+
+  beforeUpdate(event: UpdateEvent<User>): Promise<void> {
     return this.hashPassword(event.entity);
   }
 }
